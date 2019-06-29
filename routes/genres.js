8f24d79b825abff436e7f80d9752ff67bba6dc84
Guido Wagner <wagner.guido.5@gmail.com>
2019-06-29 15:02:50 -0300

    validateId middleware

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const validateId = require('../middleware/validateId')
 const admin = require('../middleware/admin')
 const auth = require('../middleware/auth')
 const { Genre, validate } = require('../models/genre')
@@ -19,7 +20,7 @@ router.post('/', auth, async (req, res) => {
   res.send(genre)
 })
 
-router.put('/:id', auth, async (req, res) => {
+router.put('/:id', [auth, validateId], async (req, res) => {
   const { error } = validate(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
@@ -40,7 +41,7 @@ router.delete('/:id', [auth, admin], async (req, res) => {
   res.send(genre)
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', validateId, async (req, res) => {
   const genre = await Genre.findById(req.params.id)
   if (!genre) return res.status(404).send('The genre Id does not exists')
   res.send(genre)
